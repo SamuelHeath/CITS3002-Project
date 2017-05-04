d73ce5915f18d02c71e748e3d2a6a380d6913055
Daedalos97 <sam.j.s.heath@gmail.com>
2017-05-04 22:58:25 +0800

    Updated .gitignore for NetBeans

---
+++ Miner/src/core/main.java
@@ -0,0 +1,16 @@
+package core;
+
+/**
+ *
+ * @author Sam
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
