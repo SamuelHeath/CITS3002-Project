a40094c9a9caa6475b702434a05fda72b70df384
Samuel Heath <Daedalos97@users.noreply.github.com>
2017-05-30 22:00:54 +0800

    Wallet fix (#4)
    * Reshuffled the Wallet
    
    * Tried to speed up miner by reducing bytes
    
    Hash the constant bytes from the header so that the miner has to use
    less bytes when it is concatonating a the bytes to do a hash.

--- Wallet/src/Wallet.java
+++ Wallet/src/Wallet.java
@@ -1,5 +1,69 @@
-public class Wallet {
-    public Wallet() {}
+
+import com.google.gson.Gson;
+import java.util.Scanner;
+import net.Message;
+
+public class Wallet implements Runnable {
+    
+    private static Message msg;
+    public static boolean update = false;
+    public static double balance;
+    private static double trans_amount;
+    private static String sender_address;
+    
+    @Override
+    public void run() {
+        
+        WalletConnection.sendMessage(new Message("RQBC;"));
+        
+        while (!update) {
+            if (!(balance == WalletIO.getBalance())) {
+                responseOccured(true); // untoggle.
+            }
+        }
+        
+        balance = WalletIO.getBalance();
+        
+        //Sends the main message required if its not an update message.
+        if (msg.getType().equals("TX")) {
+            while (!checkTransactionAmount(trans_amount)) {
+                System.out.print("Enter new transaction amount: ");
+                Scanner s = new Scanner(System.in);
+                trans_amount = s.nextDouble();
+            }
+            Transaction t = new Transaction(KeyPairGen.publicKey2String(KeyPairGen.getPublicKey()),sender_address,trans_amount);
+            t.signTransaction();
+            
+            if (t.verifySignature()) {
+                WalletConnection.sendMessage(new Message("TX;"+new Gson().toJson(t, Transaction.class)));
+            } else { System.out.println("Couldn't verify signature."); System.exit(-1); }
+        }
+    }
+    
+    /**
+     * Constructs the wallet software and then performs work.
+     * @param args                  The runtime arguments passed to the wallet.
+     */
+    public Wallet(String[] args) {
+        if (args.length == 3) {
+            msg = new Message("RQBC;");
+        } else if (args.length == 4) {
+            String[] txmsg = args[3].split(" ");
+            if (txmsg.length == 2) {
+                msg = new Message("TX;"); // Initially create a message header.
+                sender_address = txmsg[0];
+                trans_amount = Double.parseDouble(txmsg[1]);
+            } else {
+                System.out.println("Invalid Command");
+                System.exit(1);
+            }
+        } else {
+            System.out.println("You must enter atleast 3 arguments");
+            System.exit(-1);
+        }
+    }
+    
+    public static void responseOccured(boolean b) { update = b; }
     
     public static boolean checkTransactionAmount(Double amount) {
         if (amount >= 0.00000001) {
